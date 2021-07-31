ec0950ddaf8c7a6d1d10aab3ce490891641445e8
Ifan Alriansyah <fanalriansyah@gmail.com>
2021-07-31 09:17:34 +0700

    Create header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Header.scss";
+import { Container, Navbar, Nav, Image } from "react-bootstrap";
+import Logo from "../assets/img/logo.png";
+
+export default function Header() {
+  return (
+    <Navbar bg="light" variant="light">
+      <Container>
+        <Navbar.Brand>
+          <Image src={Logo} />
+        </Navbar.Brand>
+        <Nav className="me-auto">
+          <div className="header">
+            <p>Good Morning</p>
+            <h4>Hello Fellas</h4>
+          </div>
+        </Nav>
+      </Container>
+    </Navbar>
+  );
+}
