aec9f5822f363e5d0d27b9260bc601cd49b5aa42
Ifan Alriansyah <fanalriansyah@gmail.com>
2021-08-01 16:58:07 +0700

    Import Logo from asset, adding button

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,17 +1,20 @@
 import React from "react";
 import "./Homepage.scss";
-import { Container, Row, Col } from "react-bootstrap";
+import Logo from "../assets/img/lego.png";
+import { Container, Row, Col, Image, Button } from "react-bootstrap";
 
 export default function Homepage() {
   return (
-    <Container className="homepage">
+    <Container fluid className="homepage">
       <Row>
-        <Row>
-          <Col>
-            <h1>WEEKEND FROM HOME</h1>
-            <h2>Stay active with a little workout.</h2>
-          </Col>
-        </Row>
+        <Col>
+          <h1>WEEKEND FROM HOME</h1>
+          <h2>Stay active with a little workout.</h2>
+          <Image src={Logo} />
+          <Button className="button" variant="light">
+            <p>Let's Go</p>
+          </Button>
+        </Col>
       </Row>
     </Container>
   );
