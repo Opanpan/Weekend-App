b2644ea609c8ffc631f199a6a79eaab6d70eff79
Ifan Alriansyah <fanalriansyah@gmail.com>
2021-08-02 12:05:25 +0700

    Change div to Container fluid

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import "./App.scss";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Container } from "react-bootstrap";
 import Homepage from "./pages/Homepage";
 import Header from "./components/Header";
 import Definition from "./pages/Definition";
 
 function App() {
   return (
-    <div>
+    <Container fluid className="p-0">
       <section>
         <Header />
         <Homepage />
@@ -14,7 +15,7 @@ function App() {
       <section>
         <Definition />
       </section>
-    </div>
+    </Container>
   );
 }
 
