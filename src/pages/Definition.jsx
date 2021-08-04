dab759952f2d7091da741525da21650e3a7b1468
Ifan Alriansyah <fanalriansyah@gmail.com>
2021-08-04 19:43:42 +0700

    Delete Row and Col component

--- src/pages/Definition.jsx
+++ src/pages/Definition.jsx
@@ -8,42 +8,32 @@ import Testimonial from "../components/Testimonial";
 export default function Definition() {
   return (
     <Container fluid className="p-0">
-      <Row>
-        <Col className="d-flex justify-content-end definition">
-          <Image src={secondLego} />
-        </Col>
-      </Row>
-      <Row>
-        <Col className="d-flex justify-content-center">
-          <h3 className="about-definition">
-            <span style={{ color: "#0b24fb" }}>Deffinition;</span> a practice or
-            exercise to test or improve one's fitness for athletic competition,
-            ability, or performance to exhaust (something, such as a mine) by
-            working to devise, arrange, or achieve by resolving difficulties.
-            Merriam-Webster.com Dictionary.
-            <br />
-            <br />
-            <br />
-            <span style={{ color: "white" }}>-weekend team</span>
-          </h3>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <Image className="about-shape" src={ovalShape} />
-        </Col>
-      </Row>
-      <Row>
-        <Col className="d-flex justify-content-center">
-          <h1>Testimonial</h1>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          {" "}
-          <Testimonial />
-        </Col>
-      </Row>
+      <div className="definition d-flex justify-content-end">
+        <Image src={secondLego} />
+      </div>
+      <div className="about-definition d-flex justify-content-center">
+        <h3>
+          <span style={{ color: "#0b24fb" }}>Deffinition;</span> a practice or
+          exercise to test or improve one's fitness for athletic competition,
+          ability, or performance to exhaust (something, such as a mine) by
+          working to devise, arrange, or achieve by resolving difficulties.
+          Merriam-Webster.com Dictionary.
+          <br />
+          <br />
+          <br />
+          <span style={{ color: "white" }}>-weekend team</span>
+        </h3>
+      </div>
+
+      <Image
+        className="about-shape"
+        style={{ marginTop: 100 }}
+        src={ovalShape}
+      />
+      <div className="d-flex justify-content-center">
+        <h1>Testimonial</h1>
+      </div>
+      <Testimonial />
     </Container>
   );
 }
